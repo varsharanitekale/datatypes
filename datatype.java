6791fec35049510fcba51c6b5dde13a3b63c2072
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-15 16:36:58 +0530

    initial

---
+++ datatype.java
@@ -0,0 +1,19 @@
+public class datatype {
+    public static void main(String[] args){
+        int a=10;
+        Float b = 10.45550078f;
+        Double c= 1234.869849d;
+        Long e= 123455677L;
+        String r="StringType is well";
+        char s='s';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(r);
+        System.out.println(s);
+
+
+    }
+    
+}
